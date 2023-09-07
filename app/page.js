647a57aba159a4ec93dd305c34aab10376ff825c
Mohammed <najjarmedd@gmail.com>
2023-09-07 12:48:26 +0100

    responsive heading text

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 export default function home (){
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center bg-gradient-to-tr from-indigo-700 to-black">
-      <h1 className="text-white text-8xl font-bold ">COD<span className="text-green-600">Lead</span></h1>
+      <h1 className="text-white text-6xl font-bold md:text-7xl lg:text-8xl">COD<span className="text-green-600">Lead</span></h1>
       <p className="text-white mt-6">Comming soon...</p>
     </div>
   )
