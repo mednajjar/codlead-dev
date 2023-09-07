02c4506c86a23ad572bfb48f00f8cd88f7cf1481
Mohammed <najjarmedd@gmail.com>
2023-09-07 12:39:56 +0100

    homepage

---
+++ app/page.js
@@ -0,0 +1,10 @@
+
+
+export default function home (){
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center bg-gradient-to-tr from-indigo-700 to-black">
+      <h1 className="text-white text-8xl font-bold ">COD<span className="text-green-600">Lead</span></h1>
+      <p className="text-white mt-6">Comming soon...</p>
+    </div>
+  )
+}
